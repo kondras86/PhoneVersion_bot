d048af460dfa1d8d2f458aee40b8ed0bbe5cc62d
kondras86 <150272780+kondras86@users.noreply.github.com>
2025-07-22 00:40:23 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ if not TOKEN:
     raise RuntimeError("Переменная окружения TOKEN не установлена!")
 
 # URL вашего WebApp — обязательно используйте ваш реальный поддомен
-WEBAPP_URL = "https://google.com"
+WEBAPP_URL = "https://your-webapp.onrender.com*"
 
 # Создаём Flask-приложение
 app = Flask(__name__)
